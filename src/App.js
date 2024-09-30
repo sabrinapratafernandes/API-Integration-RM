8e07c9594b631f78719941b128bcfc64eade88d0
Sabrina <sabrinabuenoprata@gmail.com>
2024-09-30 16:23:02 -0300

    Adiciona tabela de dados, traduz e altera cores
    Adiciona tabela com informações dos personagens.
    Ajusta as cores e responsividade dos cards de personagens.
    Traduz os dados mapeados dos personagens para o português.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState, useCallback } from 'react';
 import './index'
 import Button from './components/Button';
 import Banner from './components/Banner';
+import translate from './translations';
 
 function App() {
   const [data, setData] = useState([]); // Estado que armazena os dados dos personagens retornados da API. Inicialmente é um array vazio.
   const [loading, setLoading] = useState(false); // Estado que indica se os dados estão sendo carregados ou não. Inicialmente falso.
   const [page, setPage] = useState(1); // Estado que armazena o número da página atual. Inicialmente começa na página 1.
   const [search, setSearch] = useState('');
+  const [visibleTable, setVisibleTable] = useState(null);
 
   const temp = () => {
     console.log('temp '+ page)
@@ -39,6 +41,15 @@ function App() {
   const searchData = data.filter(info =>
     info.name.toLowerCase().includes(search.toLocaleLowerCase())
   );
+
+  // Função para alternar a visibilidade da tabela ao clicar no personagem
+  const toggleTableVisibility = (id) => {
+    if (visibleTable === id) {
+      setVisibleTable(null);
+    } else {
+      setVisibleTable(id);
+    }
+  };
   
   return (
     <div className=''>
@@ -47,17 +58,47 @@ function App() {
     <main className='flex flex-col items-center'> 
     <Banner/>
     <input 
-          className='w-4/5 border-solid border-2 border-black rounded-lg m-2 lg:w-3/5'
+          className='p-2 w-4/5 border-solid border-2 border-black rounded-lg m-2 lg:w-3/5'
           type="text"
           placeholder="Digite o nome de um personagem..."
           value={search}
           onChange={(e) => setSearch(e.target.value)} // Atualiza o valor da busca
     />
-    <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5'>
+    <div className='grid gap-1 grid-cols-2 md:grid-cols-3 lg:grid-cols-5 p-2'>
     {searchData.map((info)=>
-    <div className='p-2 m-2 bg-green hover:bg-purple rounded flex flex-col items-center justify-center'>
+    <div 
+    key={info.id}
+    className='p-2 bg-green hover:bg-black hover:text-green rounded flex flex-col items-center justify-center cursor-pointer'
+    onClick={() => toggleTableVisibility(info.id)}>
+
     <img className='mt-4' src={info.image}/>
     <p className='p-2 text-lg font-bold'>{info.name}</p>
+  
+    <div className='p-2 md:w-full'>
+
+      {visibleTable == info.id &&(
+      <table className="table-auto border-collapse border w-full">
+        <tbody>
+          <tr>
+            <td className="border border-blue p-2">Status</td>
+            <td className="border border-blue p-2">{translate.status[info.status] || info.status}</td>
+          </tr>
+          <tr>
+            <td className="border border-blue p-2">Espécie</td>
+            <td className="border border-blue p-2">{translate.species[info.species] || info.species}</td>
+          </tr>
+          <tr>
+            <td className="border border-blue p-2">Tipo</td>
+            <td className="border border-blue p-2">{info.type || "Não especificado"}</td>
+          </tr>
+          <tr>
+            <td className="border border-blue p-2">Gênero</td>
+            <td className="border border-blue p-2">{translate.gender[info.gender] || info.gender}</td>
+          </tr>
+        </tbody>
+      </table>
+      )}
+    </div>
     </div>
     )}
     </div>
---
+++ src/translations.js
@@ -0,0 +1,19 @@
+const translate = {
+    status: {
+        Alive: 'Vivo',
+        Dead: 'Morto',
+        unknown: 'Desconhecido',
+      },
+      species: {
+        Human: 'Humano',
+        Alien: 'Alienígena',
+        unknown: 'Desconhecido',
+      },
+      gender: {
+        Male: 'Masculino',
+        Female: 'Feminino',
+        unknown: 'Desconhecido',
+      },
+};
+
+export default translate
